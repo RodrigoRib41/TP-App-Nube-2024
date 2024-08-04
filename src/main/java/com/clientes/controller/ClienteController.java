036298037f88792b1e1663f2325aea97e5814158
RodrigoRiboldi <rodrigorib41@gmail.com>
2024-08-03 23:02:27 -0300

    Add MensajeResponse

--- src/main/java/com/clientes/controller/ClienteController.java
+++ src/main/java/com/clientes/controller/ClienteController.java
@@ -1,9 +1,5 @@
 package com.clientes.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
@@ -15,11 +11,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.clientes.model.dto.ClienteDTO;
 import com.clientes.model.entity.Cliente;
+import com.clientes.model.payload.MensajeResponse;
 import com.clientes.service.ICliente;
 
 @RestController
@@ -29,57 +25,105 @@ public class ClienteController {
     private ICliente clienteService;
 
     @PostMapping("cliente")
-    @ResponseStatus(HttpStatus.CREATED)
-    public ClienteDTO create(@RequestBody ClienteDTO clienteDto){
-        Cliente clienteSave = clienteService.save(clienteDto);
-        return ClienteDTO.builder()
-        .id(clienteSave.getId())
-        .nombre(clienteSave.getNombre())
-        .correo(clienteSave.getCorreo())
-        .cuit(clienteSave.getCuit())
-        .maxdescubierto(clienteSave.getMaxdescubierto())
-        .build();
+    public ResponseEntity<?> create(@RequestBody ClienteDTO clienteDto){
+        Cliente clienteSave=null;
+        try {
+            clienteSave = clienteService.save(clienteDto);
+            return new ResponseEntity<> (MensajeResponse.builder()
+                                            .mensaje("Guardado Correctamente")
+                                            .object(ClienteDTO.builder()
+                                            .id(clienteSave.getId())
+                                            .nombre(clienteSave.getNombre())
+                                            .correo(clienteSave.getCorreo())
+                                            .cuit(clienteSave.getCuit())
+                                            .maxdescubierto(clienteSave.getMaxdescubierto())
+                                            .build())
+                                            .build()
+                                        ,HttpStatus.CREATED);
+        } catch (DataAccessException e) {
+        return new ResponseEntity<>(MensajeResponse.builder()
+                                            .mensaje(e.getMessage())
+                                            .object(null)
+                                            .build()
+                                        , HttpStatus.METHOD_NOT_ALLOWED);
     }
+}
 
-    @PutMapping("cliente")
-    @ResponseStatus(HttpStatus.CREATED)
-    public ClienteDTO update(@RequestBody ClienteDTO clienteDto){
-        Cliente clienteUpdate = clienteService.save(clienteDto);
-        return ClienteDTO.builder()
-        .id(clienteUpdate.getId())
-        .nombre(clienteUpdate.getNombre())
-        .correo(clienteUpdate.getCorreo())
-        .cuit(clienteUpdate.getCuit())
-        .maxdescubierto(clienteUpdate.getMaxdescubierto())
-        .build();
+    @PutMapping("cliente/{id}")
+    public ResponseEntity<?> update(@RequestBody ClienteDTO clienteDto, @PathVariable Integer id){
+        Cliente clienteUpdate = null;
 
+        try {
+            Cliente clienteFind= clienteService.findById(id);
+            if(clienteFind==null){
+                return new ResponseEntity<>(MensajeResponse.builder()
+                                            .mensaje("El registro no se encuentra en la base de datos")
+                                            .object(null)
+                                            .build()
+                                        ,HttpStatus.METHOD_NOT_ALLOWED);
+            
+            }
+            clienteUpdate = clienteService.save(clienteDto);
+            return new ResponseEntity<> (MensajeResponse.builder()
+                                            .mensaje("Guardado Correctamente")
+                                            .object(ClienteDTO.builder()
+                                            .id(clienteUpdate.getId())
+                                            .nombre(clienteUpdate.getNombre())
+                                            .correo(clienteUpdate.getCorreo())
+                                            .cuit(clienteUpdate.getCuit())
+                                            .maxdescubierto(clienteUpdate.getMaxdescubierto())
+                                            .build())
+                                            .build()
+                                        , HttpStatus.CREATED);
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(MensajeResponse.builder()
+                                            .mensaje(e.getMessage())
+                                            .object(null)
+                                            .build()
+                                        ,HttpStatus.METHOD_NOT_ALLOWED);
+        }
     }
 
     @DeleteMapping("cliente/{id}")
     public ResponseEntity<?> delete(@PathVariable Integer id){
-        Map<String,Object> responses = new HashMap<>();
         try {
             Cliente clienteDelete=clienteService.findById(id);
             clienteService.delete(clienteDelete);
             return new ResponseEntity<>(clienteDelete,HttpStatus.NO_CONTENT);
         } catch (DataAccessException e) {
-            responses.put("Mensaje", e.getMessage());
-            responses.put("Cliente", null);
-            return new ResponseEntity<>(responses,HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(MensajeResponse.builder()
+                                            .mensaje(e.getMessage())
+                                            .object(null)
+                                            .build()
+                                        ,HttpStatus.METHOD_NOT_ALLOWED);
         }
     }
 
     @GetMapping("cliente/{id}")
-    @ResponseStatus(HttpStatus.OK)
-    public ClienteDTO showById(@PathVariable Integer id){
+    public ResponseEntity<?> showById(@PathVariable Integer id){
             Cliente cliente = clienteService.findById(id);
-            return ClienteDTO.builder()
-            .id(cliente.getId())
-            .nombre(cliente.getNombre())
-            .correo(cliente.getCorreo())
-            .cuit(cliente.getCuit())
-            .maxdescubierto(cliente.getMaxdescubierto())
-            .build();
+
+            if(cliente==null){
+            return new ResponseEntity<>(MensajeResponse.builder()
+                                            .mensaje("El registro que intenta buscar no existe")
+                                            .object(null)
+                                            .build()
+                                        ,HttpStatus.NOT_FOUND);
+            }
+
+            return new ResponseEntity<>(MensajeResponse.builder()
+                                            .mensaje(null)
+                                            .object(ClienteDTO.builder()
+                                            .id(cliente.getId())
+                                            .nombre(cliente.getNombre())
+                                            .correo(cliente.getCorreo())
+                                            .cuit(cliente.getCuit())
+                                            .maxdescubierto(cliente.getMaxdescubierto())
+                                            .build())
+                                            .build()
+                                        ,HttpStatus.OK);
+            
 
     }
 
